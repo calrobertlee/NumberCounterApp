47ab2d6f3438944501aa4b5b02759e3e16a6d18c
Calvin Robert Lee <calvinrobertlee@gmail.com>
2022-12-06 17:43:02 -0800

    Basic Int Counter

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var number = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Spacer()
+            Text("\(number)")
+                .font(.system(size: 70))
+            HStack {
+                Button(action: {
+                    self.number -= 1
+                }, label: {
+                    Text("Remove")
+                        
+                })
+                Button(action: {
+                    self.number += 1
+                }, label: {
+                    Text("Add")
+                })
+            }
+            Spacer()
+        }
     }
 }
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
