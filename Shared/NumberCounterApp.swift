43451e89fb5c441affba0dead51bee7f3376ee84
Calvin Robert Lee <calvinrobertlee@gmail.com>
2022-12-06 17:23:41 -0800

    Initial Commit

---
+++ Shared/NumberCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberCounterApp.swift
+//  Shared
+//
+//  Created by user on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct NumberCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
