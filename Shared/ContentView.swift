ad11d42afd6febdf540c89c3066f3e81c1d8e48a
Calvin Robert Lee <calvinrobertlee@gmail.com>
2022-12-09 21:53:23 -0800

    added equals function

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,23 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var number = 0
+    @State private var secretNum: Int = 0
     
     var body: some View {
         
         VStack{
             Spacer()
+            Text("\(secretNum)").font(.system(size: 34))
             Text("\(number)").font(.system(size: 70))
             // Buttons
             HStack {
                 Button(action: {
-                    number -= 1
-                }, label: {
-                    Text("Minus")
-                })
-                Button(action: {
-                    number += 1
+                    secretNum = number
+                    number = 0
                 }, label: {
                     Text("Add")
+                })
+                Button(action: {
+                    number = number + secretNum
+                }, label: {
+                    Text("Equals")
                 })
                 Button(action: {
                     number = number * number
@@ -40,25 +43,50 @@ struct ContentView: View {
             }
             Spacer()
             HStack {
-                Button(action: {
-                    number = 1
-                }, label: {
-                    Text("1")
-                })
-                Button(action: {
-                    number = number * 10 + 2
-                }, label: {
-                    Text("2")
-                })
-                Button(action: {
-                    number = number * 10 + 3
-                }, label: {
-                    Text("3")
-                })
+                if number < 999999 {
+                    // Button 0
+                    Button(action: {
+                        number = number * 10
+                    }, label: {
+                        Text("0").font(.title)
+                    })
+                    // Button 1
+                    Button(action: {
+                        number = number * 10 + 1
+                    }, label: {
+                        Text("1").font(.title)
+                    })
+                    // Button 2
+                    Button(action: {
+                        number = number * 10 + 2
+                    }, label: {
+                        Text("2").font(.title)
+                    })
+                    // Button 3
+                    Button(action: {
+                        number = number * 10 + 3
+                    }, label: {
+                        Text("3").font(.title)
+                    })
+                } else {
+                    Text("0").font(.title).foregroundColor(.gray)
+                    Text("1").font(.title).foregroundColor(.gray)
+                    Text("2").font(.title).foregroundColor(.gray)
+                    Text("3").font(.title).foregroundColor(.gray)
+
+                }
             }
         }
     }
 }
+
+
+
+
+
+
+
+
 
 
 
