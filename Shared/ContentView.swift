f7a77cc31b9b6196348f4d74326f6ed848cfce58
Calvin Robert Lee <calvinrobertlee@gmail.com>
2022-12-11 22:47:55 -0800

    Merged v3 branch to main branch

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -2,52 +2,141 @@
 //  ContentView.swift
 //  Shared
 //
-//  Created by user on 12/6/22.
+//  Created by Calvin Robert Lee on 12/6/22.
 //
 
 import SwiftUI
 
-/// This app is a number counter, where the number is displayed at the center of screen. Based on the buttons the users presses, the value increases by 1, decreases by 1, or is set back to 0.
 struct ContentView: View {
-    // Displays a number which can increase or decrease in value.
-    @State private var number = 0
+    @State var entry: Int = 0
+    @State var operand: Int = 0
+    @State var secondOperand: Int = 0
+    @State var store: Bool = true
+    @State var additionActive: Bool = false
+    @State var subtractionActive: Bool = false
+
     
     var body: some View {
         
-        VStack{
-            Spacer()
+        VStack {
+            /* Debugging display numbers.
+             
+            Text("\(secondOperand)")
+                .font(.largeTitle).foregroundColor(.red)
+            Text("\(operand)")
+                .font(.largeTitle).foregroundColor(.orange)
+             
+             */
+            Text("\(entry)")
+                .font(.system(size: 60))
+                .foregroundColor(.black)
+                .padding()
             
-            // Displays current number.
-            Text("\(number)")
-                .font(.system(size: 70))
+            // Input buttons 0-9.
             HStack {
-                // Button labeled Remove which decreases current number by 1.
-                Button(action: {
-                    number -= 1
-                }, label: {
-                    Text("Minus")
-                })
-                // Button labeled Add which increases current number by 1.
-                Button(action: {
-                    number += 1
-                }, label: {
-                    Text("Add")
-                })
-                Button(action: {
-                    number = number * number
-                }, label: {
-                    Text("Square")
-                })
-                Button(action: {
-                    number = 0
-                }, label: {
-                    Text("Clear")
-                })
+                Button(action: { entry = entry * 10 },
+                       label: { Text("0").font(.title) })
+                Button(action: { entry = entry * 10 + 1 },
+                       label: { Text("1").font(.title) })
+                Button(action: { entry = entry * 10 + 2 },
+                       label: { Text("2").font(.title) })
+                Button(action: { entry = entry * 10 + 3 },
+                       label: { Text("3").font(.title) })
+                Button(action: { entry = entry * 10 + 4 },
+                       label: { Text("4").font(.title) })
+                Button(action: { entry = entry * 10 + 5 },
+                       label: { Text("5").font(.title) })
+                Button(action: { entry = entry * 10 + 6 },
+                       label: { Text("6").font(.title) })
+                Button(action: { entry = entry * 10 + 7 },
+                       label: { Text("7").font(.title) })
+                Button(action: { entry = entry * 10 + 8 },
+                       label: { Text("8").font(.title) })
+                Button(action: { entry = entry * 10 + 9 },
+                       label: { Text("9").font(.title) })
             }
-            Spacer()
+            
+            // Operation buttons.
+            VStack {
+                
+                // Addition button. NOTE: Add bold later.
+                Button(action: {
+                    operand = entry
+                    entry = 0 // NOTE: update feature later.
+                    additionActive = true
+                    subtractionActive = false
+                    store = true
+                }, label: {
+                    Text("Addition").font(.title3)
+                }).padding(.vertical, 2.0)
+                Button(action: {
+                    operand = entry
+                    entry = 0 // NOTE: update feature later.
+                    subtractionActive = true
+                    additionActive = false
+                    store = true
+                }, label: {
+                    Text("Subtraction").font(.title3)
+                }).padding(.vertical, 2.0)
+                
+                // Calculate button.
+                Button(action: {
+                    // Transfers values to hidden stored numbers.
+                    if store {
+                        if additionActive {
+                            secondOperand = entry
+                            entry = secondOperand + operand
+                            store = false
+                        }
+                        if subtractionActive {
+                            secondOperand = entry
+                            entry = operand - secondOperand
+                            store = false
+                        }
+                    // Allows for repeated calculation of current amount.
+                    } else {
+                        if additionActive {
+                            entry = entry + secondOperand
+                        }
+                        if subtractionActive {
+                            entry = entry - secondOperand
+                        }
+                    }
+                }, label: {
+                    Text("Calculate").font(.title3)
+                }).padding(.vertical, 2.0)
+                
+                // Reset button.
+                Button(action: {
+                    (entry, operand, secondOperand) = (0,0,0)
+                    store = true
+                    (additionActive, subtractionActive) = (false, false)
+                }, label: {
+                    Text("Reset").font(.title3)
+                }).padding(.vertical, 2.0)
+                
+            }.padding()
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
