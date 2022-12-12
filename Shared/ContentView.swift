c15ced1828a48798f2cf3e1af8adc71e60a75d52
Calvin Robert Lee <calvinrobertlee@gmail.com>
2022-12-11 22:15:35 -0800

    Added all input numbers. Added ability to continue previous calculation.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,40 +12,54 @@ struct ContentView: View {
     @State var operand: Int = 0
     @State var secondOperand: Int = 0
     @State var store: Bool = true
-    
-    // Resuable struct, planned for input buttons.
-    struct button: View {
-        var label: String
-        
-        var body: some View {
-            Text("\(label)").font(.largeTitle).foregroundColor(.black)
-        }
-    }
+
     
     var body: some View {
         
         VStack {
-            button(label: "TEST")
-            Text("\(secondOperand)").font(.largeTitle).foregroundColor(.black)
-            Text("\(operand)").font(.largeTitle).foregroundColor(.red)
-            Text("\(entry)").font(.largeTitle).foregroundColor(.blue).padding()
+//            Text("\(secondOperand)")
+//                .font(.largeTitle).foregroundColor(.black)
+//            Text("\(operand)")
+//                .font(.largeTitle).foregroundColor(.red)
+            Text("\(entry)")
+                .font(.system(size: 60))
+                .foregroundColor(.black)
+                .padding()
             
+            // Input buttons 0-9.
+            HStack {
+                Button(action: { entry = entry * 10 },
+                       label: { Text("0").font(.title) })
+                Button(action: { entry = entry * 10 + 1 },
+                       label: { Text("1").font(.title) })
+                Button(action: { entry = entry * 10 + 2 },
+                       label: { Text("2").font(.title) })
+                Button(action: { entry = entry * 10 + 3 },
+                       label: { Text("3").font(.title) })
+                Button(action: { entry = entry * 10 + 4 },
+                       label: { Text("4").font(.title) })
+                Button(action: { entry = entry * 10 + 5 },
+                       label: { Text("5").font(.title) })
+                Button(action: { entry = entry * 10 + 6 },
+                       label: { Text("6").font(.title) })
+                Button(action: { entry = entry * 10 + 7 },
+                       label: { Text("7").font(.title) })
+                Button(action: { entry = entry * 10 + 8 },
+                       label: { Text("8").font(.title) })
+                Button(action: { entry = entry * 10 + 9 },
+                       label: { Text("9").font(.title) })
+            }
+            
+            // Operation buttons.
             VStack {
-                Button(action: {
-                    entry = 10
-                }, label: {
-                    Text("input 10").font(.title3)
-                })
-                Button(action: {
-                    entry = 2
-                }, label: {
-                    Text("input 2").font(.title3)
-                })
+                // Addition button. NOTE: Add bold later.
                 Button(action: {
                     operand = entry
+                    entry = 0 // NOTE: update feature later.
                 }, label: {
                     Text("Addition").font(.title3)
-                }) // Add active bold state later
+                }).padding(.vertical, 2.0)
+                // Calculate button.
                 Button(action: {
                     if store {
                         secondOperand = entry
@@ -56,13 +70,14 @@ struct ContentView: View {
                     }
                 }, label: {
                     Text("Calculate").font(.title3)
-                })
+                }).padding(.vertical, 2.0)
+                // Reset button.
                 Button(action: {
                     (entry, operand, secondOperand) = (0,0,0)
                     store = true
                 }, label: {
                     Text("Reset").font(.title3)
-                })
+                }).padding(.vertical, 2.0)
             }.padding()
         }
     }
